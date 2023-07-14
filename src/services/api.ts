6ed9994c5f3eef25bc465b6600c50070d0fc659b
Hugo Andrade <hugoalvesandrade@hotmail.com>
2023-07-14 02:08:07 -0300

    Atualizado urlbase

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,33 +5,33 @@ import { AuthTokenError } from "@/services/errors/AuthTokenError";
 import { signOut } from "@/contexts/AuthContext";
 
 export function setUpAPIClient(ctx = undefined) {
-    let cookies = parseCookies(ctx);
+  let cookies = parseCookies(ctx);
 
-    const api = axios.create({
-        baseURL: `https://pizzaclub-c5ln.onrender.com`,
-        headers: {
-            Authorization: `Bearer ${cookies["@nextauth.token"]}`,
-        },
-    });
+  const api = axios.create({
+    baseURL: `https://pizzaclubapi.onrender.com/`,
+    headers: {
+      Authorization: `Bearer ${cookies["@nextauth.token"]}`,
+    },
+  });
 
-    api.interceptors.response.use(
-        (response) => {
-            return response;
-        },
-        (error: AxiosError) => {
-            if (error.response.status === 401) {
-                // Qualquer erro 401 (não autorizado) devemos deslogar o usuário
-                if (typeof window !== undefined) {
-                    // Chamar a função para deslogar o usuário
-                    signOut();
-                } else {
-                    return Promise.reject(new AuthTokenError());
-                }
-            }
-
-            return Promise.reject(error);
+  api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error: AxiosError) => {
+      if (error.response.status === 401) {
+        // Qualquer erro 401 (não autorizado) devemos deslogar o usuário
+        if (typeof window !== undefined) {
+          // Chamar a função para deslogar o usuário
+          signOut();
+        } else {
+          return Promise.reject(new AuthTokenError());
         }
-    );
+      }
 
-    return api;
+      return Promise.reject(error);
+    }
+  );
+
+  return api;
 }
